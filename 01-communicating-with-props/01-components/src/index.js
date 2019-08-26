6026805fe0bb65dd1addc2b67f0ee0232c2115c1
SamiraKarimi <samira.ka006@gmail.com>
2019-08-26 11:11:12 -0500

    component reuse and fixing some typo

--- 01-communicating-with-props/01-components/src/index.js
+++ 01-communicating-with-props/01-components/src/index.js
@@ -2,13 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author ="Sam"  time = "Today at 6:00PM"  comment = "Nice blog post!"/>
-      <CommentDetail author = "Alex"  time = "Today at 5:00PM"  comment = "cool blog!"/>
-      <CommentDetail author = "Jane"  time = "Today at 1:00PM"  comment = "Awsome post!"/>
+       <ApprovalCard>
+      <CommentDetail author ="Sam" 
+       time = "Today at 6:00PM" 
+       content = "Nice blog post!"
+        avatar= {faker.image.avatar()}
+        />
+        </ApprovalCard>
+
+       <ApprovalCard>
+      <CommentDetail author = "Alex"
+       time = "Today at 5:00PM" 
+       content = "cool blog!" 
+       avatar= {faker.image.avatar()}
+       />
+       </ApprovalCard>
+
+       <ApprovalCard>
+      <CommentDetail author = "Jane" 
+        time = "Today at 1:00PM" 
+        content = "Awsome post!"
+        avatar = {faker.image.avatar()}
+        />
+        </ApprovalCard>
 
     </div>
   );
