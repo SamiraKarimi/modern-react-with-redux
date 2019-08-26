e06d17a17bd99405daed8d15890b20c5085056d8
SamiraKarimi <samira.ka006@gmail.com>
2019-08-25 21:49:36 -0500

    passing props

--- 01-communicating-with-props/01-components/src/CommentDetail.js
+++ 01-communicating-with-props/01-components/src/CommentDetail.js
@@ -12,9 +12,9 @@ const CommentDetail = (props) => {
           {props.author}
         </a>
         <div className="metadata">
-          <span className="data">Today at 6:00PM</span>
+          <span className="data">{props.time}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.comment}</div>
       </div>
     </div>
   );
--- 01-communicating-with-props/01-components/src/index.js
+++ 01-communicating-with-props/01-components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author ="Sam"/>
-      <CommentDetail author = "Alex"/>
-      <CommentDetail author = "Jane"/>
+      <CommentDetail author ="Sam"  time = "Today at 6:00PM"  comment = "Nice blog post!"/>
+      <CommentDetail author = "Alex"  time = "Today at 5:00PM"  comment = "cool blog!"/>
+      <CommentDetail author = "Jane"  time = "Today at 1:00PM"  comment = "Awsome post!"/>
 
     </div>
   );
