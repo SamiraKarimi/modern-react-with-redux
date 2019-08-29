68b1865080d06f17fa97900907902adbbe664844
SamiraKarimi <samira.ka006@gmail.com>
2019-08-29 16:52:55 -0500

    determining season

--- 02-structuring-apps-with-class-based-component/seasons/src/index.js
+++ 02-structuring-apps-with-class-based-component/seasons/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
  
 
     class App extends React.Component {
-        constructor (props){
-            super(props);
-            this.state = {lat: null,errorMessage:''};
-            }
+       state = {lat:null,errorMessage:''};
 
         componentDidMount(){
             window.navigator.geolocation.getCurrentPosition(
@@ -20,11 +18,11 @@ import ReactDOM from 'react-dom';
                 return <div>Error: {this.state.errorMessage}</div>;
             }
             if(!this.state.errorMessage && this.state.lat) {
-                return <div>latitude: {this.state.lat}</div>;
+                return <SeasonDisplay lat ={this.state.lat} />
             }
             return <div>Loading!</div>;
         }
-    }
+    } 
 
 
 
