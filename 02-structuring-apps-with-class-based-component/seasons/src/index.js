80f7b0766863f1326d4b553cab136278589eca16
SamiraKarimi <samira.ka006@gmail.com>
2019-08-28 20:32:45 -0500

    updating state properties

--- 02-structuring-apps-with-class-based-component/seasons/src/index.js
+++ 02-structuring-apps-with-class-based-component/seasons/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
  
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-     position => console.log(position),
-     err => console.log(err)
-    );
-    return <div>Latitude:</div>
-    };
+
+    class App extends React.Component {
+        constructor (props){
+            super(props);
+            this.state = {lat: 40};
+            
+            window.navigator.geolocation.getCurrentPosition(
+                position => {
+                    this.setState({lat : position.coords.latitude})
+                },
+                err => console.log(err)
+            );
+        }
+        
+        render() {
+            
+            return <div>Latitude: {this.state.lat}</div>
+        }
+    }
 
 
 
