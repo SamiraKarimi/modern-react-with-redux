0d2dd1e044d2e83631b2ca1f840752f51752d4db
SamiraKarimi <samira.ka006@gmail.com>
2019-08-30 19:04:48 -0500

    add icon

--- 02-structuring-apps-with-class-based-component/seasons/src/SeasonDisplay.js
+++ 02-structuring-apps-with-class-based-component/seasons/src/SeasonDisplay.js
@@ -1,4 +1,14 @@
 import React from 'react';
+const seasonConfig = {
+    summer : {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+},
+winter: {
+    text: 'Burr it is cold',
+    iconName:'snowflake'
+}
+};
 
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
@@ -10,9 +20,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-   console.log(season);
-
-    return <div>Season Display</div>
+    const {text, iconName} = seasonConfig[season];
+    
+    return( 
+        <div>
+            <i className = { `${iconName} icon`}/>
+            <h1>{text}</h1>
+            <i className = {`${iconName} icon`}/>
+        </div>
+    
+    );
 };
 
 export default SeasonDisplay;
